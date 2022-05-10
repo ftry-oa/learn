7837d519c4fd1be513f841ac9d4876d1986255aa
suifengfengye <1527261080@qq.com>
2022-05-10 08:19:07 +0800

    add

--- FWebpack/lib/Compiler.js
+++ FWebpack/lib/Compiler.js
@@ -18,14 +18,39 @@ class Compiler {
         }
       }
     })
+    console.log('@@@@modules', this.modules)
+
+    /**
+     * 构建依赖关系图
+     {
+        'index.js': {
+          code: 'xxxx',
+          deps: { 'add.js': '绝对路径' }
+        },
+        'add.js': {
+          code: 'xxx',
+          deps: {},
+        }
+     }
+     */
+    const depsGraph = this.modules.reduce((prevValue, currentValue) => {
+      const { filePath, code, deps } = currentValue
+      prevValue[filePath] = {
+        code,
+        deps,
+      }
+      console.log('@@@@---e', code)
+      return prevValue
+    }, {})
+    console.log('@@@@', depsGraph)
   }
 
   build (filePath) {
     // 1. 解析文件内容为ast
-    const ast = getAst(entryFilePath)
+    const ast = getAst(filePath)
 
     // 2. 构建依赖
-    const deps = getDeps(ast, entryFilePath)
+    const deps = getDeps(ast, filePath)
 
     // 3. 编译文件代码
     const code = getCode(ast)
@@ -36,6 +61,31 @@ class Compiler {
       deps,
       code,
     }
+  }
+
+  genarate (depsGraph) {
+    /**
+      "use strict";
+    
+      var _add = _interopRequireDefault(require("./add.js"));
+      
+      var _todo = _interopRequireDefault(require("./todo.js"));
+      
+      function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+      
+      console.log('@@@@add', (0, _add["default"])(1, 2));
+      console.log('@@@@todo', (0, _todo["default"])('jianfehuang', 'hahahhaxxxx'));
+     */
+    const bundle = `(function(depsGraph){
+      function require(filePath){
+        eval(depsGraph[filePath].code)
+      }
+
+      require(${this.options.entry})
+    })(${JSON.stringify(depsGraph)})`
+
+
+
   }
 }
 
